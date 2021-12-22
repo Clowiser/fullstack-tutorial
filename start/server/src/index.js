2cea0c0329f938cba5a36f476c3262700203dc00
rigaud <jessica@enalps.fr>
2021-12-22 17:19:29 +0100

    Suivi du tutoriel - Etape 1

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -1,1 +1,18 @@
 require('dotenv').config();
+
+const { ApolloServer } = require('apollo-server');
+//importe la classe ApolloServer de AS
+
+const typeDefs = require('./schema');
+//importe un schéma encore non défini
+
+const server = new ApolloServer({ typeDefs });
+//création d'instance de AS et lui transmet le schéma importé via typeDefs
+
+server.listen().then(() => {
+    console.log(`
+    Server is running!
+    Listening on port 4000
+    Explore at https://studio.apollographql.com/sandbox
+  `);
+});
